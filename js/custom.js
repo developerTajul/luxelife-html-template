913d1e34ede68e0bc652c17eaac7e11a1f7d5c16
masirul-codify <masirulislam80@gmail.com>
2024-02-15 00:49:36 +0600

    merge delwar and remove some previous js

--- js/custom.js
+++ js/custom.js
@@ -87,7 +87,7 @@
         
  
         
-        
+
         var $grid = $('.isotope-grid').isotope({
             itemSelector: '.isotope-item',
             layoutMode: 'fitRows',
@@ -108,49 +108,7 @@
             $(this).addClass("active");
         });
 
-
-
-
-
-        // SERVICE DETAILS JQUERY PLUGIN- masirul
-        // faq area start
-
-        const accordionItemHeaders = document.querySelectorAll(
-            ".accordion-item-header"
-        );
-
-        accordionItemHeaders.forEach((accordionItemHeader) => {
-            accordionItemHeader.addEventListener("click", (event) => {
-                // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-
-                const currentlyActiveAccordionItemHeader = document.querySelector(
-                    ".accordion-item-header.active"
-                );
-                if (
-                    currentlyActiveAccordionItemHeader &&
-                    currentlyActiveAccordionItemHeader !== accordionItemHeader
-                ) {
-                    currentlyActiveAccordionItemHeader.classList.toggle("active");
-                    currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-                }
-                accordionItemHeader.classList.toggle("active");
-                const accordionItemBody = accordionItemHeader.nextElementSibling;
-                if (accordionItemHeader.classList.contains("active")) {
-                    accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-                } else {
-                    accordionItemBody.style.maxHeight = 0;
-                }
-            });
-        });
-
-        // faq area end
-
-        /* Home page brand logo slider testimonial
-     ===========================================*/
-        // let copy = document.querySelector(".logo-slide").cloneNode(true);
-        // document.querySelector(".slide-one").appendChild(copy);
-        // let copytwo = document.querySelector(".logo-slide").cloneNode(true);
-        // document.querySelector(".slide-two").appendChild(copytwo);
+ 
 
         // mixitup 
         var containerEl = document.querySelector('.project-gallery-wrapper');
